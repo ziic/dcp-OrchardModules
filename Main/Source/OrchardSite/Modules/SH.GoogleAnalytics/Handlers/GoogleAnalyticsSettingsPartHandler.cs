e819fb3bdcb36da5b47b43ecdb10eccd5fcdbb4c
ziic_cp <SND\ziic_cp>
2016-10-07 06:58:05 +0000

    Initial uploading

---
+++ Main/Source/OrchardSite/Modules/SH.GoogleAnalytics/Handlers/GoogleAnalyticsSettingsPartHandler.cs
@@ -0,0 +1,15 @@
+﻿using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+using Orchard.Environment.Extensions;
+using SH.GoogleAnalytics.Models;
+
+namespace SH.GoogleAnalytics.Handlers {
+	
+	[OrchardFeature("SH.GoogleAnalytics")]
+	public class GoogleAnalyticsSettingsPartHandler : ContentHandler {
+		public GoogleAnalyticsSettingsPartHandler(IRepository<GoogleAnalyticsSettingsPartRecord> repository) {
+			Filters.Add(new ActivatingFilter<GoogleAnalyticsSettingsPart>("Site"));
+			Filters.Add(StorageFilter.For(repository));
+		}
+	}
+}
