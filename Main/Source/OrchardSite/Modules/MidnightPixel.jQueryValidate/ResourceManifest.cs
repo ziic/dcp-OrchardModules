74c43d947d4134b211c14bad1616eb4414b4ee2d
ziic_cp <SND\ziic_cp>
2016-12-30 15:13:58 +0000

    dcp.Routing

---
+++ Main/Source/OrchardSite/Modules/MidnightPixel.jQueryValidate/ResourceManifest.cs
@@ -0,0 +1,33 @@
+﻿using Orchard.UI.Resources;
+
+namespace MidnightPixel.jQueryValidate
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            // jQuery Validate scripts
+            manifest.DefineScript("jQueryValidate")
+                .SetUrl("jquery.validate.min.js", "jquery.validate.js")
+                .SetVersion("1.16.0")
+                .SetDependencies("jQuery")
+                .SetCdn("//ajax.aspnetcdn.com/ajax/jquery.validate/1.16.0/jquery.validate.min.js", "//ajax.aspnetcdn.com/ajax/jquery.validate/1.16.0/jquery.validate.js", true);
+
+            // Additional Methods
+            manifest.DefineScript("jQueryValidate_AdditionalMethods")
+                .SetUrl("additional-methods.min.js", "additional-methods.js")
+                .SetVersion("1.16.0")
+                .SetDependencies("jQueryValidate")
+                .SetCdn("//ajax.aspnetcdn.com/ajax/jquery.validate/1.16.0/additional-methods.min.js", "//ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/additional-methods.js", true);
+
+            manifest.DefineScript("jquery.validate.unobtrusive")
+                .SetUrl("jquery.validate.unobtrusive.js", "jquery.validate.unobtrusive.min.js")
+                .SetVersion("5.2.3")
+                .SetDependencies("jQuery", "jQueryValidate")
+                .SetCdn("//ajax.aspnetcdn.com/ajax/mvc/5.2.3/jquery.validate.unobtrusive.min.js", "//ajax.aspnetcdn.com/ajax/mvc/5.2.3/jquery.validate.unobtrusive.js", true); ;
+
+        }
+    }
+}
