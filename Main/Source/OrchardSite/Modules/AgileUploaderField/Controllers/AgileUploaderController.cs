ae1547bd4b7852665aee1afaa171e3ddd7b45c68
ziic_cp <SND\ziic_cp>
2017-05-22 07:42:06 +0000

    fix: AgileUploadField: files allow new: AgileUploadField: resort dependencies

--- Main/Source/OrchardSite/Modules/AgileUploaderField/Controllers/AgileUploaderController.cs
+++ Main/Source/OrchardSite/Modules/AgileUploaderField/Controllers/AgileUploaderController.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Orchard;
-using Orchard.FileSystems.Media;
-using Orchard.Media.Models;
-using Orchard.Media.Services;
+using Orchard.ContentManagement;
+using Orchard.MediaLibrary.Models;
+using Orchard.MediaLibrary.Services;
 
 namespace AgileUploaderField.Controllers
 {
     public class AgileUploaderController : Controller
     {
-        private readonly IMediaService _mediaService;
+        private readonly IMediaServiceAdapter _mediaService;
 
         public AgileUploaderController(IMediaServiceAdapter mediaService)
         {
-            _mediaService = (IMediaService)mediaService;
+            _mediaService = mediaService;
         }
 
         [HttpPost]
@@ -31,36 +30,12 @@ namespace AgileUploaderField.Controllers
                 foreach (string keyPostedFile in postedFiles)
                 {
                     var postedFile = postedFiles[keyPostedFile];
+                    if (postedFile == null)
+                        continue;
+
                     if (_mediaService.FileAllowed(postedFile))
                     {
-                        var fileName = Path.GetFileName(postedFile.FileName);
-                        var uniqueFileName = fileName;
-                        try
-                        {
-                            // try to create the folder before uploading a file into it
-                            _mediaService.CreateFolder(null, agileUploaderMediaFolder);
-                        }
-                        catch
-                        {
-                            // the folder can't be created because it already exists, continue
-                        }
-                        var filesInFolder = _mediaService.GetMediaFiles(agileUploaderMediaFolder).ToList();
-                        var found =
-                            filesInFolder.Any(
-                                f => 0 == String.Compare(fileName, f.Name, StringComparison.OrdinalIgnoreCase));
-                        var index = 0;
-                        while (found)
-                        {
-                            index++;
-                            uniqueFileName = String.Format("{0}-{1}{2}", Path.GetFileNameWithoutExtension(fileName),
-                                                           index,
-                                                           Path.GetExtension(fileName));
-                            found =
-                                filesInFolder.Any(
-                                    f => 0 == String.Compare(uniqueFileName, f.Name, StringComparison.OrdinalIgnoreCase));
-                        }
-                        results.Add(_mediaService.UploadMediaFile(agileUploaderMediaFolder, uniqueFileName,
-                                                                  postedFile.InputStream, false));
+                        results.Add(_mediaService.UploadMediaFile(agileUploaderMediaFolder, postedFile));
                     }
                 }
             }
@@ -68,93 +43,50 @@ namespace AgileUploaderField.Controllers
             
         }
     }
-    
-    public class MediaServiceAdapter: IMediaServiceAdapter, IMediaService
+
+    public class MediaServiceAdapter : IMediaServiceAdapter
     {
-        private readonly MediaService _mediaService;
+        private readonly IOrchardServices _orchardServices;
+        private readonly IMediaLibraryService _mediaLibraryService;
 
-        public MediaServiceAdapter(IStorageProvider storageProvider, IOrchardServices orchardServices)
+        public MediaServiceAdapter(IOrchardServices orchardServices, IMediaLibraryService mediaLibraryService)
         {
-            _mediaService = new MediaService(storageProvider, orchardServices);
-        }
-
-        public string GetPublicUrl(string relativePath)
-        {
-            return _mediaService.GetPublicUrl(relativePath);
-        }
-
-        public string GetMediaPublicUrl(string mediaPath, string fileName)
-        {
-            return _mediaService.GetMediaPublicUrl(mediaPath, fileName);
-        }
-
-        public IEnumerable<MediaFolder> GetMediaFolders(string relativePath)
-        {
-            return _mediaService.GetMediaFolders(relativePath);
-        }
-
-        public IEnumerable<MediaFile> GetMediaFiles(string relativePath)
-        {
-            return _mediaService.GetMediaFiles(relativePath);
+            _orchardServices = orchardServices;
+            _mediaLibraryService = mediaLibraryService;
         }
 
         public void CreateFolder(string relativePath, string folderName)
         {
-            _mediaService.CreateFolder(relativePath, folderName);
+            _mediaLibraryService.CreateFolder(relativePath, folderName);
         }
 
-        public void DeleteFolder(string folderPath)
+        public string UploadMediaFile(string folderPath, HttpPostedFileBase postedFile)
         {
-            _mediaService.DeleteFolder(folderPath);
-        }
+            if (!FileAllowed(postedFile))
+                throw new InvalidOperationException("Fiel type is not allowed");
+            var uniqueFileName = _mediaLibraryService.GetUniqueFilename(folderPath, postedFile.FileName);
 
-        public void RenameFolder(string folderPath, string newFolderName)
-        {
-            _mediaService.RenameFolder(folderPath, newFolderName);
-        }
-
-        public void DeleteFile(string folderPath, string fileName)
-        {
-            _mediaService.DeleteFile(folderPath, fileName);
-        }
-
-        public void RenameFile(string folderPath, string currentFileName, string newFileName)
-        {
-            _mediaService.RenameFile(folderPath, currentFileName, newFileName);
-        }
-
-        public void MoveFile(string fileName, string currentPath, string newPath)
-        {
-            _mediaService.MoveFile(fileName, currentPath, newPath);
-        }
-
-        public string UploadMediaFile(string folderPath, HttpPostedFileBase postedFile, bool extractZip)
-        {
-            return _mediaService.UploadMediaFile(folderPath, postedFile, extractZip);
-        }
-
-        public string UploadMediaFile(string folderPath, string fileName, byte[] bytes, bool extractZip)
-        {
-            return _mediaService.UploadMediaFile(folderPath, fileName, bytes, extractZip);
-        }
-
-        public string UploadMediaFile(string folderPath, string fileName, Stream inputStream, bool extractZip)
-        {
-            return _mediaService.UploadMediaFile(folderPath, fileName, inputStream, extractZip);
+            return _mediaLibraryService.UploadMediaFile(folderPath, uniqueFileName, postedFile.InputStream);
         }
 
         public bool FileAllowed(HttpPostedFileBase postedFile)
         {
-            return _mediaService.FileAllowed(postedFile);
+            var settings = _orchardServices.WorkContext.CurrentSite.As<MediaLibrarySettingsPart>();
+            var allowedExtensions = (settings.UploadAllowedFileTypeWhitelist ?? "")
+                .Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.StartsWith(".")).ToList();
+
+            return !(allowedExtensions.Any() && !allowedExtensions.Any(e => postedFile.FileName.EndsWith(e, StringComparison.OrdinalIgnoreCase)));
+
         }
 
-        public bool FileAllowed(string fileName, bool allowZip)
-        {
-            return _mediaService.FileAllowed(fileName, allowZip);
-        }
+       
     }
 
     public interface IMediaServiceAdapter : IDependency
     {
+        bool FileAllowed(HttpPostedFileBase postedFile);
+        string UploadMediaFile(string folderPath, HttpPostedFileBase postedFile);
+       
     }
 }
